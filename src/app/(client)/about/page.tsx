19f27917baf30115d9fd0da217e58925959e36a8
munier-tech <muniry399@gmail.com>
2025-11-20 17:28:53 +0300

    done

--- src/app/(client)/about/page.tsx
+++ src/app/(client)/about/page.tsx
@@ -29,7 +29,7 @@ export default function About() {
       }
     },
     { 
-      name: "Muniir Mohamed Yusuf", 
+      name: "Eng Muniir Mohamed Yusuf", 
       role: "Developer and tech lead", 
       customImage: hello2,
       socialLinks: {
